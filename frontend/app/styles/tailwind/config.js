5e7e34bdc7d9cb7e343746404b6710a3f01b8826
Chelsea Shaw <chelshaw.dev@gmail.com>
2023-03-07 08:48:08 -0600

    Homepage chaos

--- frontend/app/styles/tailwind/config.js
+++ frontend/app/styles/tailwind/config.js
@@ -13,9 +13,9 @@ module.exports = {
         lightest: '#C1F0FF',
         lighter: '#87E2FF',
         main: '#2ECDFF',
-        dark: '#1DA9D4',
-        darker: '#0882A9',
-        darkest: '#0F546A',
+        dark: '#00A1E0',
+        darker: '#0070A8',
+        darkest: '#004368',
       },
       green: {
         lightest: '#B0F7C7',
@@ -42,6 +42,43 @@ module.exports = {
         darkest: '#865B00',
       }
     },
+    extend: {
+      dropShadow: {
+        glow: '0 35px 35px rgba(255, 255, 255, 0.55)'
+      },
+      keyframes: {
+        leftToRight: {
+          '0%': { left: '-50%' },
+          '100%': { left: '150%' },
+        },
+        rightToLeft: {
+          '0%': { left: '150%' },
+          '100%': { left: '-50%' },
+        },
+        bigSmall: {
+          '0%, 100%': { transform: 'scale(100%)' },
+          '50%': { transform: 'scale(350%)' },
+        },
+        step: {
+          '0%, 100%': { transform: 'rotate(-20deg)' },
+          '50%': { transform: 'rotate(40deg)' },
+        },
+        bicycle: {
+          '0%': { left: '100%', transform: 'rotate(40deg)' },
+          '35%': { left: '65%', transform: 'rotate(35deg)' },
+          '45%': { left: '55%', transform: 'rotate(0deg)' },
+          '100%': { left: '-20%' },
+        }
+      },
+      animation: {
+        'l-r': 'leftToRight 10s ease-in infinite',
+        'r-l': 'rightToLeft 8s ease-in-out infinite',
+        'big-small': 'bigSmall 6s ease-in-out infinite',
+        'step': 'step 4s ease infinite',
+        'bicycle': 'bicycle 8s linear infinite',
+        'kayak': 'spin 5s ease infinite'
+      }
+    }
   },
   plugins: [],
 };
