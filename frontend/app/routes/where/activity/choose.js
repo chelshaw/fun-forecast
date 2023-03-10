c777431f7a5c13627768aafa56216e550e1ddc5c
Chelsea Shaw <chelshaw.dev@gmail.com>
2023-03-10 14:11:55 -0600

    Update activity/choose page with full location object

--- frontend/app/routes/where/activity/choose.js
+++ frontend/app/routes/where/activity/choose.js
@@ -1,8 +1,12 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class WhereActivityChooseRoute extends Route {
+  @service location;
+
   model() {
     const { loc_ref } = this.paramsFor('where.activity');
-    return loc_ref;
+    // TODO: Handle error case if cannot find location
+    return this.location.getById(loc_ref);
   }
 }
