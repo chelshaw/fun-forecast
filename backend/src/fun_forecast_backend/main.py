d2f7e4cea647c44c9f76c14d49b58bd670a9aa62
Shayaan Jagtap <shayaan.jagtap@gmail.com>
2023-03-24 22:18:10 -0500

    logging structure in place

--- backend/src/fun_forecast_backend/main.py
+++ backend/src/fun_forecast_backend/main.py
@@ -5,6 +5,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from fun_forecast_backend.activity_schema.activity import get_activity_schema_by_key
 from fun_forecast_backend.activity_schema.dataclasses import ActivitySchema
+from fun_forecast_backend.core.standard_logger import get_logger
+
+logger = get_logger()
 
 app = FastAPI(title="Fun Forecast Backend")
 
@@ -16,8 +19,6 @@ app.add_middleware(
     allow_headers=["*"],
     max_age=3600,
 )
-
-# logger = get_logger()
 
 
 @app.on_event("startup")
@@ -38,6 +39,7 @@ async def get_location_suggestions(search: str) -> Dict[str, Any]:
 
 @app.get("/api/v0/go/{verb}/{lat},{long}")
 async def get_activity_forecast(verb: str, lat: float, long: float) -> Dict[str, Any]:
+    logger.debug(f"get_activity_forecast called, inputs verb={verb}, lat={lat}, long={long}")
     try:
         # fetch activity schema
         schema: ActivitySchema = get_activity_schema_by_key(verb)
@@ -49,4 +51,5 @@ async def get_activity_forecast(verb: str, lat: float, long: float) -> Dict[str,
         # return answer
         return {}
     except:
+        logger.error(f"get_activity_forecast failed, inputs verb={verb}, lat={lat}, long={long}")
         return {}
