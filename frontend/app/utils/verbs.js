27b23d624eb37216899c05333a7dfd290cfe3a79
Chelsea Shaw <chelshaw.dev@gmail.com>
2023-04-02 23:24:12 -0500

    update verbs and icon options in frontend

--- frontend/app/utils/verbs.js
+++ frontend/app/utils/verbs.js
@@ -1,14 +1,23 @@
-export const verb_icons = {
-  run: '🏃🏾',
-  hike: '🥾',
-  bicycle: '🚲',
-  swim: '🏊🏽',
-  kayak: '🛶',
-  climb: '🧗🏽',
-  motorcycle: '🏍',
-  sunbathe: '👙',
-};
+export const allActivities = [
+  { verb: 'bicycle', icon: '🚲', active: true },
+  { verb: 'climb', icon: '🧗🏽', active: true },
+  { verb: 'golf', icon: '⛳️', active: true },
+  { verb: 'hike', icon: '🥾', active: true },
+  { verb: 'kayak', icon: '🛶', active: true },
+  { verb: 'motorcycle', icon: '🏍', active: true },
+  { verb: 'pickleball', icon: '🏓', active: true },
+  { verb: 'picnic', icon: '🧺', active: true },
+  { verb: 'run', icon: '🏃🏾', active: true },
+  { verb: 'skateboard', icon: '🛹', active: true },
+  { verb: 'swim', icon: '🏊🏽', active: true },
+  { verb: 'tennis', icon: '🎾', active: true },
+  { verb: 'walk', icon: '👟', active: true },
+];
+
+export function getVerbIcon(verb) {
+  return allActivities.findBy(v => v.verb === verb)?.icon || '';
+}
 
 export default function allowedVerbs() {
-  return Object.keys(verb_icons);
+  return allActivities.filter(v => v.active).map(v => v.verb)
 }
