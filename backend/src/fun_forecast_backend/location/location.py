6d0d7bc5f2fcc8623f313db7dd54423ab40cb3ef
Shayaan Jagtap <31865035+shayaanjagtap@users.noreply.github.com>
2023-04-05 17:19:06 -0500

    Location API cleaned up, preparing secret read for deployment testing (#21)

--- backend/src/fun_forecast_backend/location/location.py
+++ backend/src/fun_forecast_backend/location/location.py
@@ -1,28 +1,31 @@
 import os
 import requests
 from requests import Response
-from fastapi import HTTPException
 from typing import Dict, Any
+
 import dacite
+from fastapi import HTTPException
 
 from fun_forecast_backend.core.standard_logger import get_logger
 from .dataclasses import GeocodeResponse
 
+
 logger = get_logger()
 
+mapbox_api_key: str = ''
 if 'MAPBOX_API_KEY' in os.environ:
-    mapboxKey = os.environ['MAPBOX_API_KEY']
+    mapbox_api_key = os.environ['MAPBOX_API_KEY']
 else:
-    logger.warn('MAPBOX_API_KEY not set, location search endpoint will not work')
+    logger.warning('MAPBOX_API_KEY env variable not set, location search endpoint will not work')
 
 
-def get_location_suggestions(keyword: str) -> Dict[str, Any]:
+def get_location_suggestions(keyword: str) -> GeocodeResponse:
     try:
-        url = f"https://api.mapbox.com/geocoding/v5/mapbox.places/{keyword}.json?country=us&limit=5&access_token={mapboxKey}"
+        url = f"https://api.mapbox.com/geocoding/v5/mapbox.places/{keyword}.json?country=us&limit=5&access_token={mapbox_api_key}"
 
         r: Response = requests.get(url)
         if r.status_code != 200:
-            logger.error(f"error response in geocode for {keyword} - {r.status_code}: {r.text}")
+            logger.error(f"Error response in geocode for {keyword} - {r.status_code}: {r.text}")
             raise HTTPException(
                 status_code=404, detail="Something went wrong. Please try again later.")
 
@@ -34,7 +37,7 @@ def get_location_suggestions(keyword: str) -> Dict[str, Any]:
                 status_code=400, detail="Could not find results. Please try a different keyword.")
         
         return response
-    except:
-        logger.error("get_location_suggestions failed for keyword {keyword}")
+    except Exception as e:
+        logger.error(f"get_location_suggestions failed for keyword: {keyword} - {e}")
         raise HTTPException(
             status_code=500, detail="Something went wrong. Please try again later.")
