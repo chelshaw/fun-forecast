13e4cabb2a3824a000faa6beeb749594e59c5eb2
Shayaan Jagtap <shayaan.jagtap@gmail.com>
2023-04-06 19:41:31 -0500

    testing version change?

--- .build/git_update.sh
+++ .build/git_update.sh
@@ -45,7 +45,7 @@ else
 fi
 
 # create new tag
-NEW_TAG="v$VNUM1.$VNUM2.$VNUM3"
+NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
 echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
 
 # get current hash and see if it already has a tag
