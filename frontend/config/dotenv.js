9954398661c7e0b05bd679a3bd7793d2aee74427
Chelsea Shaw <chelshaw.dev@gmail.com>
2023-04-07 10:50:18 -0500

    Try to get dotenv not to trigger in CI

--- frontend/config/dotenv.js
+++ frontend/config/dotenv.js
@@ -5,12 +5,13 @@
 const path = require('path');
 
 module.exports = function (env) {
+  console.log('FE environment:', env)
   return {
+    // Only enable env file for development
+    enabled: env === 'development',
     clientAllowedKeys: ['FF_BETACODE'],
     fastbootAllowedKeys: [],
     failOnMissingKey: true,
     path: path.join(path.dirname(__dirname), '.env'),
-    // Vercel doesn't have access to .env file, so disable for env
-    enabled: env === 'development',
   };
 };
