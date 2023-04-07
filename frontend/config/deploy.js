965712d14976b7ea5d4a903508eccac6842f3776
Chelsea Shaw <chelshaw.dev@gmail.com>
2023-04-07 11:13:45 -0500

    Remove staging vars

--- frontend/config/deploy.js
+++ frontend/config/deploy.js
@@ -24,18 +24,20 @@ module.exports = function (deployTarget) {
   };
 
   if (deployTarget === 'staging') {
-    ENV.build.environment = 'production';
-    ENV.s3.bucket = process.env.STAGING_BUCKET;
-    ENV.s3.region = process.env.AWS_REGION;
-    // Staging env doesn't use cloudfront
-    ENV.pipeline.activateOnDeploy = false;
+    // We don't actually use staging at the moment
+    // Values will need to be updated when we do
+    ENV.pipeline.activateOnDeploy = false
+    ENV.build.environment = 'development';
+    ENV.s3.bucket = 'none'; 
+    ENV.s3.region = 'none';
+    ENV.cloudfront.distribution = 'none';
   }
 
   if (deployTarget === 'production') {
     ENV.build.environment = 'production';
-    ENV.s3.bucket = process.env.PROD_BUCKET;
+    ENV.s3.bucket = process.env.AWS_BUCKET;
     ENV.s3.region = process.env.AWS_REGION;
-    ENV.cloudfront.distribution = process.env.PROD_DISTRIBUTION;
+    ENV.cloudfront.distribution = process.env.AWS_DISTRIBUTION;
   }
 
   // Note: if you need to build some configuration asynchronously, you can return
