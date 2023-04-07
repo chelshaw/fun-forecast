fdd7c5394d5a03cad2dfe27f9686e10ba319ffa3
Chelsea Shaw <chelshaw.dev@gmail.com>
2023-04-07 10:55:00 -0500

    disable dotenv

--- frontend/config/dotenv.js
+++ frontend/config/dotenv.js
@@ -8,7 +8,7 @@ module.exports = function (env) {
   console.log('FE environment:', env)
   return {
     // Only enable env file for development
-    enabled: env === 'development',
+    enabled: false,
     clientAllowedKeys: ['FF_BETACODE'],
     fastbootAllowedKeys: [],
     failOnMissingKey: true,
